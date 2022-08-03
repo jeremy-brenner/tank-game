04a1467aa0d276388321434d8eb748a0f8519c83
Jeremy J Brenner <jeremyjbrenner@gmail.com>
2022-08-03 13:10:46 -0500

    clear out hello world stuff

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ApiCheck />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ApiCheck from './components/ApiCheck.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ApiCheck
   }
 }
 </script>
---
+++ frontend/src/components/ApiCheck.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Api Check
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ApiCheck',
+  props: {}
+}
+</script>
+
+<style scoped>
+</style>
